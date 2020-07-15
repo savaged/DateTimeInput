0857e6c150f50d5bf4c337601425fffbd380ab13
David Savage <savagedavid@gmail.com>
2020-07-15 14:02:11 +0100

    first commit

---
+++ DateTimeInput/Models/SampleModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DateTimeInput.Models
+{
+    public class SampleModel : INotifyPropertyChanged
+    {
+        private DateTime _theDateTime;
+        private DateTime _theDate;
+
+        public DateTime TheDateTime
+        {
+            get => _theDateTime;
+            set
+            {
+                _theDateTime = value;
+                NotifyPropertyChanged();
+                _theDate = GetDateOnly(value);
+                NotifyPropertyChanged(nameof(TheDate));
+            }
+        }
+
+        public DateTime TheDate
+        {
+            get => _theDate;
+            set
+            {
+                _theDate = GetDateOnly(value);
+                NotifyPropertyChanged();
+                _theDateTime = new DateTime(
+                    _theDate.Year,
+                    _theDate.Month,
+                    _theDate.Day,
+                    TheDateTime.Hour,
+                    TheDateTime.Minute,
+                    0,
+                    0,
+                    DateTimeKind.Unspecified);
+                NotifyPropertyChanged(nameof(TheDateTime));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private DateTime GetDateOnly(DateTime value)
+        {
+            return new DateTime(
+                    value.Year,
+                    value.Month, 
+                    value.Day);           
+        }
+
+    }
+}
---
+++ DateTimeInput/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DateTimeInput.Models;
+using DateTimeInput.Utils;
+using System;
+using System.Windows.Input;
+
+namespace DateTimeInput.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            ModelObject = new SampleModel
+            {
+                TheDateTime = DateTime.Now
+            };
+        }
+
+        public SampleModel ModelObject { get; set; }
+
+        public ICommand ExitCmd { get { return new RelayCommand(OnExitApp, () => true); } }
+
+
+        private void OnExitApp()
+        {
+            System.Windows.Application.Current.MainWindow.Close();
+        }
+    }
+}
